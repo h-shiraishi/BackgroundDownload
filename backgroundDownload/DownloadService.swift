366d2b1b5bf5873ef0f95a9d28528ab034c101e3
edelweiss <edelweiss199@yahoo.co.jp>
2016-10-27 09:19:18 +0900

    再開処理のiOS10対応

--- backgroundDownload/DownloadService.swift
+++ backgroundDownload/DownloadService.swift
@@ -207,7 +207,7 @@ public class DownloadService: NSObject {
         //再開データを読み込む
         var resumeData : Data?
         do {
-            resumeData = try Data(contentsOf: fileUrl)
+            resumeData = self.correctResumeData(try Data(contentsOf: fileUrl))
         } catch {
             self.addDownload(id: id, url: url)
             return
@@ -284,5 +284,113 @@ public class DownloadService: NSObject {
                 })
         
         self.requests[String(id)] = request
+    }
+    
+    private func correct(requestData data: Data?) -> Data? {
+        guard let data = data else {
+            return nil
+        }
+        if NSKeyedUnarchiver.unarchiveObject(with: data) != nil {
+            return data
+        }
+        guard let archive = (try? PropertyListSerialization.propertyList(from: data, options: [.mutableContainersAndLeaves], format: nil)) as? NSMutableDictionary else {
+            return nil
+        }
+        // Rectify weird __nsurlrequest_proto_props objects to $number pattern
+        var k = 0
+        while ((archive["$objects"] as? NSArray)?[1] as? NSDictionary)?.object(forKey: "$\(k)") != nil {
+            k += 1
+        }
+        var i = 0
+        while ((archive["$objects"] as? NSArray)?[1] as? NSDictionary)?.object(forKey: "__nsurlrequest_proto_prop_obj_\(i)") != nil {
+            let arr = archive["$objects"] as? NSMutableArray
+            if let dic = arr?[1] as? NSMutableDictionary, let obj = dic["__nsurlrequest_proto_prop_obj_\(i)"] {
+                dic.setObject(obj, forKey: "$\(i + k)" as NSString)
+                dic.removeObject(forKey: "__nsurlrequest_proto_prop_obj_\(i)")
+                arr?[1] = dic
+                archive["$objects"] = arr
+            }
+            i += 1
+        }
+        if ((archive["$objects"] as? NSArray)?[1] as? NSDictionary)?.object(forKey: "__nsurlrequest_proto_props") != nil {
+            let arr = archive["$objects"] as? NSMutableArray
+            if let dic = arr?[1] as? NSMutableDictionary, let obj = dic["__nsurlrequest_proto_props"] {
+                dic.setObject(obj, forKey: "$\(i + k)" as NSString)
+                dic.removeObject(forKey: "__nsurlrequest_proto_props")
+                arr?[1] = dic
+                archive["$objects"] = arr
+            }
+        }
+        /* I think we have no reason to keep this section in effect
+         for item in (archive["$objects"] as? NSMutableArray) ?? [] {
+         if let cls = item as? NSMutableDictionary, cls["$classname"] as? NSString == "NSURLRequest" {
+         cls["$classname"] = NSString(string: "NSMutableURLRequest")
+         (cls["$classes"] as? NSMutableArray)?.insert(NSString(string: "NSMutableURLRequest"), at: 0)
+         }
+         }*/
+        // Rectify weird "NSKeyedArchiveRootObjectKey" top key to NSKeyedArchiveRootObjectKey = "root"
+        if let obj = (archive["$top"] as? NSMutableDictionary)?.object(forKey: "NSKeyedArchiveRootObjectKey") as AnyObject? {
+            (archive["$top"] as? NSMutableDictionary)?.setObject(obj, forKey: NSKeyedArchiveRootObjectKey as NSString)
+            (archive["$top"] as? NSMutableDictionary)?.removeObject(forKey: "NSKeyedArchiveRootObjectKey")
+        }
+        // Reencode archived object
+        let result = try? PropertyListSerialization.data(fromPropertyList: archive, format: PropertyListSerialization.PropertyListFormat.binary, options: PropertyListSerialization.WriteOptions())
+        return result
+    }
+    
+    private func getResumeDictionary(_ data: Data) -> NSMutableDictionary? {
+        // In beta versions, resumeData is NSKeyedArchive encoded instead of plist
+        var iresumeDictionary: NSMutableDictionary? = nil
+        if #available(iOS 10.0, OSX 10.12, *) {
+            var root : AnyObject? = nil
+            let keyedUnarchiver = NSKeyedUnarchiver(forReadingWith: data)
+            
+            do {
+                root = try keyedUnarchiver.decodeTopLevelObject(forKey: "NSKeyedArchiveRootObjectKey") ?? nil
+                if root == nil {
+                    root = try keyedUnarchiver.decodeTopLevelObject(forKey: NSKeyedArchiveRootObjectKey)
+                }
+            } catch {}
+            keyedUnarchiver.finishDecoding()
+            iresumeDictionary = root as? NSMutableDictionary
+            
+        }
+        
+        if iresumeDictionary == nil {
+            do {
+                iresumeDictionary = try PropertyListSerialization.propertyList(from: data, options: PropertyListSerialization.ReadOptions(), format: nil) as? NSMutableDictionary;
+            } catch {}
+        }
+        
+        return iresumeDictionary
+    }
+    
+    private func correctResumeData(_ data: Data?) -> Data? {
+        //データの修正はiOS10以上で行う
+        if !self.laterThanEqualOS(version: 10.0) {
+            return data
+        }
+        
+        let kResumeCurrentRequest = "NSURLSessionResumeCurrentRequest"
+        let kResumeOriginalRequest = "NSURLSessionResumeOriginalRequest"
+        
+        guard let data = data, let resumeDictionary = getResumeDictionary(data) else {
+            return nil
+        }
+        
+        resumeDictionary[kResumeCurrentRequest] = correct(requestData: resumeDictionary[kResumeCurrentRequest] as? Data)
+        resumeDictionary[kResumeOriginalRequest] = correct(requestData: resumeDictionary[kResumeOriginalRequest] as? Data)
+        
+        let result = try? PropertyListSerialization.data(fromPropertyList: resumeDictionary, format: PropertyListSerialization.PropertyListFormat.xml, options: PropertyListSerialization.WriteOptions())
+        return result
+    }
+    
+    /**
+     @param version 指定したバージョン
+     指定したバージョン以上の場合にtrue
+    */
+    private func laterThanEqualOS(version : Float) -> Bool {
+        let systemVersion = UIDevice.current.systemVersion
+        return (systemVersion as NSString).floatValue >= version
     }
 }
